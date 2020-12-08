2af789a3bcc464c52172a01daaea4c7d506969c7
gandhiparth16 <69316051+gandhiparth16@users.noreply.github.com>
2020-12-08 12:43:21 +0530

    custom validation-1

--- src/App.js
+++ src/App.js
@@ -1,107 +1,176 @@
 import React from "react";
 import "./styles.css";
+import Input from './Input';
 
 class App extends React.Component {
   state = {
-    name: "",
-    license: "",
-    email: "",
-    age: "",
-    isSubmitted: false
+    orderForm: {
+        name: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Full Name'
+            },
+            value: '',
+            validation: {
+                required: true
+            },
+            valid: false,
+            touched: false
+        },
+        street: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Street'
+            },
+            value: '',
+            validation: {
+                required: true
+            },
+            valid: false,
+            touched: false
+        },
+        zipCode: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'number',
+                placeholder: 'ZIP Code'
+            },
+            value: '',
+            validation: {
+                required: true,
+                minLength: 5,
+                maxLength: 5
+            },
+            valid: false,
+            touched: false
+        },
+        country: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Country'
+            },
+            value: '',
+            validation: {
+                required: true
+            },
+            valid: false,
+            touched: false
+        },
+        email: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'email',
+                placeholder: 'Your E-Mail'
+            },
+            value: '',
+            validation: {
+                required: true,
+                pattern:"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$"
+            },
+            valid: false,
+            touched: false
+        },
+        deliveryMethod: {
+            elementType: 'select',
+            elementConfig: {
+                options: [
+                    {value: 'fastest', displayValue: 'Fastest'},
+                    {value: 'cheapest', displayValue: 'Cheapest'}
+                ]
+            },
+            value: '',
+            validation:{
+              required: false
+            },
+            valid: true
+        }
+    },
+    formIsValid: false
+}
+
+orderHandler = ( event ) => {
+  event.preventDefault();
+  this.setState( { loading: true } );
+  const formData = {};
+  for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+  }
+
+ 
+}
+
+checkValidity(value, rules) {
+  let isValid = true;
+  
+  if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+  }
+
+  if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid
+  }
+
+  if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid
+  }
+
+  return isValid;
+}
+
+inputChangedHandler = (event, inputIdentifier) => {
+  const updatedOrderForm = {
+      ...this.state.orderForm
   };
-  onInput = (event, id) => {
-    console.log(id);
-    const inputText = event.target.value;
-    this.setState({ id: inputText });
-    console.log(this.state);
+  const updatedFormElement = { 
+      ...updatedOrderForm[inputIdentifier]
   };
-  render() {
+  updatedFormElement.value = event.target.value;
+  updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+  updatedFormElement.touched = true;
+  updatedOrderForm[inputIdentifier] = updatedFormElement;
+  
+  let formIsValid = true;
+  for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+  }
+  this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
+}
+
+    render () {
+      const formElementsArray = [];
+      for (let key in this.state.orderForm) {
+          formElementsArray.push({
+              id: key,
+              config: this.state.orderForm[key]
+          });
+          console.log(formElementsArray);
+      }
+      let form = (
+          <form onSubmit={this.orderHandler}>
+              {formElementsArray.map(formElement => (
+                  <Input 
+                      key={formElement.id}
+                      elementType={formElement.config.elementType}
+                      elementConfig={formElement.config.elementConfig}
+                      value={formElement.config.value}
+                      invalid={!formElement.config.valid}
+                      shouldValidate={formElement.config.validation}
+                      touched={formElement.config.touched}
+                      changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+              ))}
+              <button
+            style={{ display: "flex", margin: "auto", alignSelf: "center" }}
+            disabled={this.state.formIsValid}
+          >Submit</button>
+          </form>
+      );
     return (
-      // const email = document.getElementById('email');
-      // console.log(this.email.validity);
-      //if(this.email.validity.valid) {
-      //const disabled=true;
-      //}
       <>
         <h1 style={{ textAlign: "center" }}>Sign up</h1>
-        <form>
-          <p>
-            Name:
-            <input
-              id="name"
-              placeholder="Full Name"
-              name="FullName"
-              required
-              onChange={(id) => this.onInput(id)}
-            />
-            <span>*</span>
-          </p>
-          <fieldset>
-            <legend>
-              Do you have a driver's license?
-              <abbr
-                id="licence"
-                title="This field is mandatory"
-                aria-label="required"
-              >
-                *
-              </abbr>
-            </legend>
-            <input
-              type="radio"
-              required
-              id="driver"
-              value="yes"
-              onChange={(id) => this.onInput(id)}
-            />
-            <label htmlFor="r1">Yes</label>
-            <input
-              type="radio"
-              required
-              id="driver"
-              value="no"
-              onChange={(id) => this.onInput(id)}
-            />
-            <label htmlFor="r2">No</label>
-          </fieldset>
-
-          <p>
-            email-id:
-            <input
-              type="text"
-              id="email"
-              name="email"
-              placeholder="email addresss"
-              onChange={(id) => this.onInput(id)}
-              required
-              pattern="^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
-            />
-            *
-          </p>
-
-          <p>
-            age:
-            <input
-              type="number"
-              min="12"
-              max="120"
-              step="1"
-              id="age"
-              name="age"
-              onChange={(id) => this.onInput(id)}
-              placeholder="age"
-              pattern="\d+"
-            />
-          </p>
-          {/*<p>Comments:<textarea id="comments" name="msg" maxlength="140" rows="5" cols="15"/></p>*/}
-
-          <button
-            style={{ display: "flex", margin: "auto", alignSelf: "center" }}
-            disabled={this.disabled}
-          >
-            Submit
-          </button>
-        </form>
+        {form}
       </>
     );
   }
